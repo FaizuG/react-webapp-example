5792318185cea643d5fb8a1eef6df63f673835c9
FaizuG <faizansf2004@gmail.com>
2024-06-10 22:46:02 +0530

    Update FormatCode.tsx

--- components/FormatCode.tsx
+++ components/FormatCode.tsx
@@ -1,7 +1,7 @@
 export default function FormatCode() {
   return (
     <pre className="w-full break-words overflow-x-auto whitespace-pre-wrap">
-      {JSON.stringify(window.Telegram?.WebApp, null, 2)}
+      {JSON.stringify(window.Telegram?.WebApp, my first app, 2)}
     </pre>
   )
 }
